656ef4ec460cd3b4cafdff8047dcce9b81516e44
kalyanitentu <153392425+kalyanitentu@users.noreply.github.com>
2024-05-28 17:10:36 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -32,9 +32,6 @@ if ingredients_list:
             values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
         time_to_insert = st.button('Submit Order')
         if time_to_insert:
-    session.sql(my_insert_stmt).collect()
+session.sql(my_insert_stmt).collect()
     
 st.success('Your Smoothie is ordered!', icon="✅")
-
-
-
